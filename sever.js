5611810fa25d8bc74bc666f5dab140f0419c2abb
HaMinhcomet <100187061+HTNMinh1910@users.noreply.github.com>
2023-03-23 18:37:07 +0700

    update_login_signup

--- sever.js
+++ sever.js
@@ -8,12 +8,29 @@ const path = require("path");
 app.engine('hbs', hbs.engine());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/src/views'));
-console.log(path.join(__dirname, '/src/views'));
 app.listen(8080);
 
-app.get('/', function(req, res){
-  res.render('home', {showTitle: true});
+app.get('/login', function(req, res){
+  res.render('login');
 });
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.get('/list', (req, res) => {
+  res.render('list');
+});
+
+  http.createServer(function (req, res) {
+  fs.readFile('./src/css/list.css', function(err, data) {
+    res.writeHead(200, {'Content-Type': 
+    'text/css'
+  });
+    res.write(data);
+    return res.end();
+  });
+}).listen(8030);
 
   http.createServer(function (req, res) {
   fs.readFile('./src/css/reset.css', function(err, data) {
